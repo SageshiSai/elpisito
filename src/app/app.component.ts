c99898f41e6fb691ad7792936ce675271c09fe1c
SageshiSai <saikonogameryt@gmail.com>
2025-03-19 12:55:30 +0100

    Integracion de paginas header y footer y rutas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MenuPrincipalComponent } from "./shared/components/menu-principal/menu-principal.component";
+import { HeaderComponent } from "./shared/components/header/header.component";
+import { FooterComponent } from "./shared/components/footer/footer.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MenuPrincipalComponent, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+  {
+    path: '',
+    loadChildren: () => import('./pages/content/content.routes').then( m => m.CONTENT_ROUTES )
+  }
+
+];
